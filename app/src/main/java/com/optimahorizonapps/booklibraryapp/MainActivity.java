d5454d58f623500934a3284ee4c4bf8cdc086184
leonardo1313 <leonardo.novak5@gmail.com>
2021-09-27 11:40:48 +0200

    AddBookActivity added - layout for second activity set

--- app/src/main/java/com/optimahorizonapps/booklibraryapp/MainActivity.java
+++ app/src/main/java/com/optimahorizonapps/booklibraryapp/MainActivity.java
@@ -1,14 +1,34 @@
 package com.optimahorizonapps.booklibraryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView book_RecyclerView;
+    private FloatingActionButton addBook_Button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        book_RecyclerView = findViewById(R.id.book_recyclerView);
+        addBook_Button = findViewById(R.id.add_floatingActionButton);
+
+        addBook_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddBookActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
